24754f640d9ff32dc6459b227cab753da1dfb976
Louis Gadza <109475790+lgadza@users.noreply.github.com>
2023-09-09 15:25:27 +0200

    change applicant url

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ import {
   unauthorizedErrorHandler,
 } from "./errorHandlers.js"
 import guardiansRouter from "./api/guardians/index.js"
-import applicantRouter from "./api/users/index.js"
+import userRouter from "./api/users/index.js"
 import addressRouter from "./api/address/index.js"
 import applicationRouter from "./api/applications/index.js"
 import guardianTypeRouter from "./api/guardians/guardian_type.js"
@@ -36,10 +36,10 @@ server.use(bodyParser.json())
 
 // ********************************** ENDPOINTS ****************************************
 
-server.use("/applicants",applicantRouter)
+server.use("/users",userRouter)
 server.use("/address", addressRouter)
 server.use("/guardians",guardiansRouter)
-server.use("/applicants/files",fileRouter)
+server.use("/users/files",fileRouter)
 server.use("/applications",applicationRouter)
 server.use("/guardians/types",guardianTypeRouter)
 server.use("/ai",AiRouter)
